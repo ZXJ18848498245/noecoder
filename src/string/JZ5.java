eb6db1bca9beb58e840eaed046536f7d8b69c375
zxj <zxj123@163.com>
2020-07-18 23:10:34 +0800

    //栈模拟队列

---
+++ src/string/JZ5.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.Stack;
+//使用两个栈模拟队列操作
+public class JZ5 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack1.empty()&&stack2.empty()){
+            throw new RuntimeException("队列为空！");
+        }
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+
+    }
+}
