9a2aad75f956b9ae7f4cf29cac1005cb6ad30e29
zxj <zxj123@163.com>
2020-07-19 23:41:37 +0800

    //数组旋转90度

---
+++ src/array/CM6.java
@@ -0,0 +1,24 @@
+package array;
+
+public class CM6 {
+    public int[][] transformImage(int[][] mat, int n) {
+        // write code here
+        if(mat == null){
+            return null;
+        }
+        int temp;
+        for(int i = 0;i<n/2;i++)
+            for(int j = 0;j<n;j++){
+                temp = mat[i][j];
+                mat[i][j]=mat[n-i-1][j];
+                mat[n-i-1][j]=temp;
+            }
+        for(int i = 0;i<n;i++)
+            for(int j=0;j<i;j++){
+                temp = mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i] = temp;
+            }
+        return mat;
+    }
+}
